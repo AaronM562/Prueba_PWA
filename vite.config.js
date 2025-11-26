234c05463bce988613572b12dc513e48bc417565
AaronM562 <trebol560@gmail.com>
2025-11-26 17:49:43 -0600

    PWA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/multiplo-divisores/",
+  base: "/multiplo-divisores/index.html",
   plugins: [vue()],
 });
